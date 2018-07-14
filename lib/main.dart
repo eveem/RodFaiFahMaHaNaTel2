c9620a0337102235b1b12c49c574019c8735b02a
salamtamp <salamtam.p@gmail.com>
2018-07-15 01:19:16 +0700

    Add CSS to confirm screen

--- lib/main.dart
+++ lib/main.dart
@@ -162,9 +162,9 @@ class ConfirmScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     String appTitle = 'RodFaiFahMaHaNaTel2 ❤';
     String text1 = 'จุดนัดพบอยู่ที่สถานี';
-    String text2 = 'หมอชิต';
+    String text2 = '\" หมอชิต \"';
     String text3 = 'คุณประหยัดไปได้';
-    String text4 = 'xxx บาท';
+    String text4 = '58 บาท';
     
     TextStyle appTitleStyle = new TextStyle (
       fontSize: 20.0,
@@ -173,23 +173,49 @@ class ConfirmScreen extends StatelessWidget {
     );
 
     TextStyle appTextStyle = new TextStyle (
+      fontSize: 32.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.blue,
+    );
+
+    TextStyle appValueStyle = new TextStyle (
+      fontSize: 36.0,
+      fontWeight: FontWeight.w900,
+      color: Colors.blue,
+    );
+
+    TextStyle buttonConfirmTextStyle = new TextStyle (
+      fontSize: 20.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.white,
+    );
+
+    TextStyle buttonCancelTextStyle = new TextStyle (
       fontSize: 20.0,
       fontWeight: FontWeight.w400,
       color: Colors.blue,
     );
 
     Widget buttonConfirm = new RaisedButton(
-      child: const Text('ตกลง'),
+      child: Text(
+        'ตกลง',
+        style: buttonConfirmTextStyle,
+      ),
+      color: Colors.blue,
       onPressed: (){
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => ResultScreen()),
+          MaterialPageRoute(builder: (context) => WaitingScreen()),
         );
       },
     );
 
     Widget buttonCancel = new RaisedButton(
-      child: const Text('ยกเลิก'),
+      child: Text(
+        'ยกเลิก',
+        style: buttonCancelTextStyle,
+      ),
+      color: Colors.white,
       onPressed: (){
         Navigator.push(
           context,
@@ -198,21 +224,57 @@ class ConfirmScreen extends StatelessWidget {
       },
     );
 
-
     return Scaffold(
       appBar: AppBar(
         title: Text(appTitle, style: appTitleStyle),
       ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
+      body: Container(
+        margin: new EdgeInsets.all(20.0),
         child: new Column(
           children: [
-            Text(text1, style: appTextStyle),
-            Text(text2, style: appTextStyle),
-            Text(text3, style: appTextStyle),
-            Text(text4, style: appTextStyle),
-            buttonConfirm,
-            buttonCancel,
+            Expanded(
+              child: new Card(
+                child: new Column(
+                  children: [
+                    Container(
+                      margin: new EdgeInsets.only(top: 60.0),
+                    ),
+                    Text(text1, style: appTextStyle),
+                    Container(
+                      margin: new EdgeInsets.only(top: 10.0),
+                    ),
+                    Text(text2, style: appValueStyle),
+                    Container(
+                      margin: new EdgeInsets.only(top: 50.0),
+                    ),
+                    Text(text3, style: appTextStyle),
+                    Container(
+                      margin: new EdgeInsets.only(top: 10.0),
+                    ),
+                    Text(text4, style: appValueStyle),
+                    Container(
+                      margin: new EdgeInsets.only(top: 10.0),
+                    ),
+                  ],  
+                ),
+              ),
+            ),
+            Container(
+              margin: new EdgeInsets.only(top: 10.0),
+            ),
+            new ButtonTheme(
+              minWidth: double.infinity,
+              height: 45.0,
+              child: buttonConfirm
+            ),
+            Container(
+              margin: new EdgeInsets.only(top: 10.0),
+            ),
+            new ButtonTheme(
+              minWidth: double.infinity,
+              height: 45.0,
+              child: buttonCancel
+            ),
           ],
         )
       )
