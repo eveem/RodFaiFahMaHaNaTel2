9fc398d4382ea2cd6c342d5ddb9f28e1b160a33d
salamtamp <salamtam.p@gmail.com>
2018-07-14 23:47:37 +0700

    Add confirm screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: new FirstScreen(),
+    home: new MatchingScreen(),
   ));
 }
 
-class FirstScreen extends StatefulWidget {
+class MatchingScreen extends StatefulWidget {
   @override
   _State createState() => _State();
 }
@@ -30,7 +30,7 @@ class ResultScreen extends StatelessWidget {
   }
 }
 
-class _State extends State<FirstScreen> {
+class _State extends State<MatchingScreen> {
   String _value = null;
   List<String> _values = new List<String>();
 
@@ -48,6 +48,26 @@ class _State extends State<FirstScreen> {
     });
   }
   Widget build(BuildContext context) {
+
+
+    Column buildButtonColumn(String label) {
+      return Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.blue,
+              ),
+            )
+          )
+        ]
+      );
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('RodFaiFahMaHaNaTel2 ❤'),
@@ -56,11 +76,8 @@ class _State extends State<FirstScreen> {
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
-            Text(
-              'ตอนนี้คุณอยู่สถานี',
-            ),
-            Text(
-              '" สยาม "',
+            Text('ตอนนี้คุณอยู่สถานี'),
+            Text('" สยาม "',
               style: TextStyle(
                 fontSize: 20.0,
                 color: Colors.green,
@@ -90,9 +107,81 @@ class _State extends State<FirstScreen> {
                   MaterialPageRoute(builder: (context) => ResultScreen()),
                 );
               },
-            )
+            ),
+            new RaisedButton(
+              child: const Text('Go to Confirm Screen'),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ConfirmScreen()),
+                );
+              },
+            ),
           ],
         ),
+      )
+    );
+  }
+}
+
+class ConfirmScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String appTitle = 'RodFaiFahMaHaNaTel2 ❤';
+    String text1 = 'จุดนัดพบอยู่ที่สถานี';
+    String text2 = 'หมอชิต';
+    String text3 = 'คุณประหยัดไปได้';
+    String text4 = 'xxx บาท';
+    
+    TextStyle appTitleStyle = new TextStyle (
+      fontSize: 20.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.blue,
+    );
+
+    TextStyle appTextStyle = new TextStyle (
+      fontSize: 20.0,
+      fontWeight: FontWeight.w400,
+      color: Colors.blue,
+    );
+
+    Widget buttonConfirm = new RaisedButton(
+      child: const Text('ตกลง'),
+      onPressed: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ResultScreen()),
+        );
+      },
+    );
+
+    Widget buttonCancel = new RaisedButton(
+      child: const Text('ยกเลิก'),
+      onPressed: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MatchingScreen()),
+        );
+      },
+    );
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(appTitle, style: appTitleStyle),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Column(
+          children: [
+            Text(text1, style: appTextStyle),
+            Text(text2, style: appTextStyle),
+            Text(text3, style: appTextStyle),
+            Text(text4, style: appTextStyle),
+            buttonConfirm,
+            buttonCancel,
+          ],
+        )
       )
     );
   }
